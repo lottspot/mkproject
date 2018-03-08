f4fd7e47e68691c42ae1b92c2585395540f45c02
James Lott <james@lottspot.com>
2018-03-08 16:52:11 -0500

    Do not treat asset search as a special object (should be implemented as a loader)

--- mkproject/core.py
+++ mkproject/core.py
@@ -2,14 +2,11 @@ from . import assets
 from . import project
 
 class Core():
-    def __init__(self, asset_loader, asset_search, project_dumper, project_renderer):
+    def __init__(self, asset_loader, project_dumper, project_renderer):
         self._loader = asset_loader
-        self._search = asset_search
         self._dumper = project_dumper
         self._renderer = project_renderer
     def load_assets(self, location): pass
-    def find_and_load_assets(self, search_term): pass
     def render_project(self, cfg): pass
     def dump_project(self, location): pass
     def run(self, render_cfg, load_location, dump_location): pass
-    def find_and_run(self, render_cfg, search_term, dump_location): pass
