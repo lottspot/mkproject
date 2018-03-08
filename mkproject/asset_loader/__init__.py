5340799c2db9b29ca2eff99759208901be041cb7
James Lott <james@lottspot.com>
2018-03-08 17:58:32 -0500

    Declare locaction value as internal to BaseAssetLoader

--- mkproject/asset_loader/__init__.py
+++ mkproject/asset_loader/__init__.py
@@ -4,11 +4,11 @@ LoaderError = AssetLoaderError
 
 class BaseAssetLoader():
     def __init__(self, location):
-        self.location = location
+        self._location = location
     def __repr__(self):
-        return '{}({})'.format(self.__name__, str(self.location))
+        return '{}({})'.format(self.__name__, str(self._location))
     def __str__(self):
-        return str(self.location)
+        return str(self._location)
     def load(self, pack):
         raise NotImplementedError()
 
