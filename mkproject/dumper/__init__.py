a98f044546997e16f1d8c36ffa88396d6ac79188
James Lott <james@lottspot.com>
2018-03-09 16:05:01 -0500

    Add internal methods to BaseDumper

--- mkproject/dumper/__init__.py
+++ mkproject/dumper/__init__.py
@@ -1,6 +1,10 @@
 class BaseDumper():
     def __init__(self, location):
         self._location = location
+    def __repr__(self):
+        return '{}({})'.format(self.__name__, str(self._location))
+    def __str__(self):
+        return str(self._location)
     def dump(self, pack):
         raise NotImplementedError()
 
