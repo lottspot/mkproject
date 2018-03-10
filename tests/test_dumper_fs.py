36582d7ee25668253fe969354aa2d5e6e74b7e19
James Lott <james@lottspot.com>
2018-03-10 11:19:49 -0500

    Refactor test_dumper_fs to use only python Mocks

--- tests/test_dumper_fs.py
+++ tests/test_dumper_fs.py
@@ -4,44 +4,28 @@ from mkproject import DumperError
 from unittest.mock import MagicMock
 from mkproject.dumper.fs import FSDumper
 
-class MockAssetPack():
-    def __init__(self, assets=()):
-        self.assets = assets
-    def paths(self):
-        paths = []
-        for path, data in self.assets:
-            paths.append(path)
-        return paths
-    def data(self, dpath):
-        for path, data in self.assets:
-            if dpath == path:
-                return data
-
 class TestFSDumper(unittest.TestCase):
     def setUp(self):
         self.dumper = FSDumper('/dev/null')
         self.dumper._Path = MagicMock()
-        self.pack = MockAssetPack()
+        self.pack = MagicMock()
     def test_dumper_fs_dumped_pack(self):
-        assets = (
-            ('a/1', 'data1'),
-            ('2',   'data2'),
-            ('a/3', 'data3')
-        )
         path = MagicMock()
-        self.pack.assets = assets
+        self.pack.paths.return_value = ('a','b','c')
+        self.pack.data.side_effect = ('data-a', 'data-b', 'data-c')
         self.dumper._Path.return_value = path
         self.dumper.dump(self.pack)
-        self.dumper._Path.called_any(path, 'a/1')
-        self.dumper._Path.called_any(path, '2')
-        self.dumper._Path.called_any(path, 'a/3')
-        path.write_bytes.called_any('data1')
-        path.write_bytes.called_any('data2')
-        path.write_bytes.called_any('data3')
+        self.dumper._Path.called_any(path, 'a')
+        self.dumper._Path.called_any(path, 'b')
+        self.dumper._Path.called_any(path, 'c')
+        path.write_bytes.called_any('data-a')
+        path.write_bytes.called_any('data-b')
+        path.write_bytes.called_any('data-c')
     def test_dumper_fs_dumped_pack_withparents(self):
         path = MagicMock()
         path.write_bytes.side_effect = [FileNotFoundError, path]
-        self.pack.assets = [('a/1', 'data')]
+        self.pack.paths.return_value = ('a',)
+        self.pack.data.side_effect = ('data',)
         self.dumper._Path.return_value = path
         self.dumper.dump(self.pack)
         path.parent.mkdir.assert_called_once_with(parents=True)
@@ -54,7 +38,7 @@ class TestFSDumper(unittest.TestCase):
         base.mkdir.assert_called_once()
     def test_dumper_fs_location_nonempty(self):
         base = MagicMock()
-        base.iterdir.return_value = ['1', '2']
+        base.iterdir.return_value = ['a', 'b']
         self.dumper._Path.side_effect = [base]
         try:
             self.dumper.dump(self.pack)
@@ -66,7 +50,8 @@ class TestFSDumper(unittest.TestCase):
     def test_dumper_fs_dump_enverror(self):
         path = MagicMock()
         path.write_bytes.side_effect = EnvironmentError
-        self.pack.assets = [('a/1', 'data')]
+        self.pack.paths.return_value = ('a',)
+        self.pack.data.side_effect = ('data',)
         self.dumper._Path.return_value = path
         try:
             self.dumper.dump(self.pack)
@@ -76,6 +61,7 @@ class TestFSDumper(unittest.TestCase):
         raise RuntimeError('Unexpected test pass')
     def test_dumper_fs_data_encode(self):
         data = MagicMock()
-        self.pack.assets = [('a/1', data)]
+        self.pack.paths.return_value = ('a',)
+        self.pack.data.side_effect = (data,)
         self.dumper.dump(self.pack)
         data.encode.assert_called_once()
